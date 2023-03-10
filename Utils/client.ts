805cb860008f064e8a59be20dc895b32aff2be75
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-03-10 18:22:38 +0100

    update utils

--- Utils/client.ts
+++ Utils/client.ts
@@ -13,3 +13,5 @@ export const client = sanityClient({
 
 const builder = imageUrlBuilder(client)
 export const urlFor = (source: any) => builder.image(source)
+//THIS COMMENT IS TO VERIFY THE IMPACT OF THIS KEBORD ON MY TYPING CAPABILITIS SO FAR IT IS FUCKING BAD THIS IS A SECOND TESTING AND IT IS GOING SO WELL. THATS UNESPECTED
+
